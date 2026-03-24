75e61305751a0fe5aed2297c8b9c6f9de07a70da
Nilupul Nishan <nilupulofficial2000@gmail.com>
2026-03-25 00:00:46 +0530

    feat: basic chat with pure python

---
+++ 1-introduction/1-basic.py
@@ -0,0 +1,24 @@
+import os
+
+from openai import AzureOpenAI
+
+
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+
+completion = client.chat.completions.create(
+    model=os.getenv("AZURE_GPT4O_MINI_DEPLOYMENT"),
+    messages=[
+        {"role": "system", "content": "You're a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a limerick about the Python programming language.",
+        },
+    ],
+)
+
+response = completion.choices[0].message.content
+print(response)
